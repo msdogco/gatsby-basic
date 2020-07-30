3ea24432b35732a35b24957afbd0afd6d5ac8fa0
Charlie Bevis <charliebevis@gmail.com>
2020-07-30 13:13:45 -0500

    add a sticky header that only shows when you scroll up

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Headroom from "react-headroom"
 import { Link } from "gatsby"
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
@@ -9,15 +10,17 @@ const ListLink = props => (
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `auto`, maxWidth: 650, padding: `0 1rem` }}>
-      <header style={{ marginBottom: `1.5rem` }}>
+      <Headroom style={{ backgroundColor: "white"}}>
+        <div style={{maxWidth: 650, margin: "auto"}} >
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>Main Squeeze Dog Co.</h3>
         </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
+        <ul style={{ listStyle: `none`, float: `right`, margin: 0 }}>
           <ListLink to="/">Home</ListLink>
           <ListLink to="/blog">Blog</ListLink>
         </ul>
-      </header>
+        </div>
+      </Headroom>
       {children}
     </div>
   )
