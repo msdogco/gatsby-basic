957134258437594c1902e8eb33fc5c43be5596fe
Charlie Bevis <charliebevis@gmail.com>
2020-08-13 12:31:14 -0500

    styling tweaks from pairing

--- src/components/mail-signup-form.js
+++ src/components/mail-signup-form.js
@@ -10,11 +10,11 @@ export default function MailSignupForm() {
     <div id="mc_embed_signup">
     <form action="https://dog.us5.list-manage.com/subscribe/post?u=69e6dfcf98b779919bf901b5a&amp;id=fbf946b3c1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
         <div id="mc_embed_signup_scroll">
-    	<label htmlFor="mce-EMAIL">Subscribe</label>
+    	<label htmlFor="mce-EMAIL">Bark to Sign up!</label>
     	<input type="email" defaultValue="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
         {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
         <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true"><input type="text" name="b_69e6dfcf98b779919bf901b5a_fbf946b3c1" tabIndex="-1" defaultValue=""/></div>
-            <div className="clear"><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
+        <div className="clear"><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
         </div>
     </form>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,12 @@ import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
+import MailSignupForm from "../components/mail-signup-form"
 
 export default function Home({ data }) {
   return (
   <Layout fullWidth={true} >
-    <div class='section tube-in-use'
+    <div class='section'
         >
         <div class='constrained-width'>
           <h1>The Original Treat Tube</h1>
@@ -17,25 +18,34 @@ export default function Home({ data }) {
           <h3>Main Squeeze was made for you!</h3>
           <p>Main Squeeze lets you use trainer-tested techniques to conquer the fear that drives his bad behavior.</p>
           <p>Use Main Squeeze to help your dog be the good dog you know she is inside.</p>
-          <h4>Compared to store-bought training treats, Main Squeeze:</h4>
-          <ul>
-          <li>is easy to prepare - no more cutting up bite-sized treats</li>
-          <li>quickly reinforces good behavior - no more fumbling in your pocket for a treat</li>
-          <li>lets you choose the filling - use your dog's favorite high-value treat</li>
-          <li>works with gloves - makes winter training easy</li>
-          </ul>
+        </div>
+        <div class='tube-in-use-wrapper'>
+          <img class="tube-in-use-image" src="/tubeInUse.svg"/>
+          <div class="tube-in-use-bullets">
+            <h4>Compared to store-bought training treats, Main Squeeze:</h4>
+              <ul>
+              <li>is easy to prepare - no more cutting up bite-sized treats</li>
+              <li>quickly reinforces good behavior - no more fumbling in your pocket for a treat</li>
+              <li>lets you choose the filling - use your dog's favorite high-value treat</li>
+              <li>works with gloves - makes winter training easy</li>
+              </ul>
+           </div>
+           <div class="tube-in-use-image" />
         </div>
       </div>
-          <div class='section section-dark'>
-            <div class='constrained-width'>
-              <h3>Working Towards a Better Future</h3>
-              <p>Main Squeeze is a flexible product: it works just as well for a puppy learning her way in the world as it does for a troubled rescue crippled by anxiety when meeting other dogs. Regardless of your dogs story, Main Squeeze shares an optimistic vision about what your dog's future might bring.</p>
-              <p>Caring for a traumatized or reactive dog can be immensely difficult: your dog doesn't have anyone but you, and your dog might make your life hard with constant barking, growling and lunging. You love your dog, but your life would be easier without him. To get rid of him would be a death sentence, so you're stuck with an almost-intolerable roommate.</p>
-              <p>This is an early ad concept we decided not to produce for Main Squeeze:</p>
-              <img src='/calmAndEasy.svg' alt="a calm and easy dog life"/>
-              <p>Even if it's not very polished, and it doesn't warrant promotion, this is a vision of why we persevere with our sometimes aggravating dogs. The founding motivation of Main Squeeze is to help reactive dogs recover through counter-conditioning, and to help puppy-training go as smoothly as possible. We may not all reach a point of having a "calm and easy dog life," but we can work to get a little closer to achieving that dream.</p>
-          </div>
+      <div class='section section-dark'>
+      <MailSignupForm/>
       </div>
+      <div class='section'>
+        <div class='constrained-width'>
+          <h3>Working Towards a Better Future</h3>
+          <p>Main Squeeze is a flexible product: it works just as well for a puppy learning her way in the world as it does for a troubled rescue crippled by anxiety when meeting other dogs. Regardless of your dogs story, Main Squeeze shares an optimistic vision about what your dog's future might bring.</p>
+          <p>Caring for a traumatized or reactive dog can be immensely difficult: your dog doesn't have anyone but you, and your dog might make your life hard with constant barking, growling and lunging. You love your dog, but your life would be easier without him. To get rid of him would be a death sentence, so you're stuck with an almost-intolerable roommate.</p>
+          <p>This is an early ad concept we decided not to produce for Main Squeeze:</p>
+          <img src='/calmAndEasy.svg' alt="a calm and easy dog life"/>
+          <p>Even if it's not very polished, and it doesn't warrant promotion, this is a vision of why we persevere with our sometimes aggravating dogs. The founding motivation of Main Squeeze is to help reactive dogs recover through counter-conditioning, and to help puppy-training go as smoothly as possible. We may not all reach a point of having a "calm and easy dog life," but we can work to get a little closer to achieving that dream.</p>
+      </div>
+  </div>
     </Layout>
   )
 }
