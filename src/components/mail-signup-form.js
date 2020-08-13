4b81858e09fedbf9d0f2d33a7ac913164587be01
Charlie Bevis <charliebevis@gmail.com>
2020-08-13 17:36:06 -0500

    'bark to sign up'

--- src/components/mail-signup-form.js
+++ src/components/mail-signup-form.js
@@ -14,7 +14,7 @@ export default function MailSignupForm() {
     	<input type="email" defaultValue="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
         {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
         <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true"><input type="text" name="b_69e6dfcf98b779919bf901b5a_fbf946b3c1" tabIndex="-1" defaultValue=""/></div>
-        <div className="clear"><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
+        <div className="clear"><input type="submit" value="Woof!" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
         </div>
     </form>
     </div>
