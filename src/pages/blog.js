5437dbf80b9fc963142dfede4c7b1001d90dcb97
Charlie Bevis <charliebevis@gmail.com>
2020-08-22 18:33:15 -0500

    remove leftover post from tutorial

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,7 +15,7 @@ export default function Blog({ data }) {
         >
           The Main Squeeze Blog
         </h1>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Post{data.allMarkdownRemark.totalCount > 1 ? "s" : ""}</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <Link
