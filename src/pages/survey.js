5f5668024972be03b6f21aafa7e0fbb830ea5593
Charlie Bevis <charliebevis@gmail.com>
2021-02-14 17:28:45 -0600

    add a survey at /survey

---
+++ src/pages/survey.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Layout } from "../components/layout"
+
+export default function Survey() {
+    return (
+            <Layout>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfDvcTDbxT3MafH147igGp37lPNk546ASoS30_Q8kOXy6LiRQ/viewform?embedded=true" title="Main Squeeze Prototype Survey, v.0" width="640" height="1165" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </Layout>
+    )
+}
